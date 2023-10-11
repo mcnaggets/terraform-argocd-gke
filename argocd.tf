37e278db2e227863d2f57e897e5b1ac5da546154
Maksim Prabarshchuk <mcnaggets@gmail.com>
2023-10-11 15:23:16 +0200

    version = "5.46.7"

--- argocd.tf
+++ argocd.tf
@@ -30,7 +30,7 @@ resource "helm_release" "argocd" {
   name             = "argocd"
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
-  version          = "5.21.1"
+  version          = "5.46.7"
   namespace        = "argocd"
   create_namespace = true
 
